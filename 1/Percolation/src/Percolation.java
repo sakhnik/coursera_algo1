0506c6ae86e1badbb0e67a3f63b001f2d3834c1d
Anatolii Sakhnik <sakhnik@gmail.com>
2017-07-13 08:57:44 +0300

    Fix after first submission

--- 1/Percolation/src/Percolation.java
+++ 1/Percolation/src/Percolation.java
@@ -1,6 +1,5 @@
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.Arrays;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,80 +14,83 @@ import java.util.Arrays;
  */
 public class Percolation {
 
-	private int n;
-	private int upper, lower;
-	private Boolean[] isOpen;
-	private int nOpen;
-	private WeightedQuickUnionUF uf;
+    private final int n;
+    private final int upper, lower;
+    private boolean[] isOpen;
+    private int nOpen;
+    private final WeightedQuickUnionUF uf;
 
     public Percolation(int n) {               // create n-by-n grid, with all sites blocked
-		if (n <= 0)
-			throw new IllegalArgumentException();
-		this.n = n;
-		upper = n*n;
-		lower = upper + 1;
-		nOpen = 0;
+        if (n <= 0)
+            throw new IllegalArgumentException();
+        this.n = n;
+        upper = n*n;
+        lower = upper + 1;
+        nOpen = 0;
 
-		isOpen = new Boolean[n*n];
-		Arrays.fill(isOpen, false);
+        isOpen = new boolean[n*n];
+		for (int i = 0; i < isOpen.length; ++i)
+			isOpen[i] = false;
 
-		uf = new WeightedQuickUnionUF(n*n + 2);
-		for (int i = 0; i != n; ++i) {
-			uf.union(upper, i);
-			uf.union(n*n - i - 1, lower);
-		}
-	}
+        uf = new WeightedQuickUnionUF(n*n + 2);
+        for (int i = 0; i != n; ++i) {
+            uf.union(upper, i);
+            uf.union(n*n - i - 1, lower);
+        }
+    }
 
-	private int getIdx(int row, int col) {
-		if (row < 1 || col < 1 || row > n || col > n)
-			throw new IllegalArgumentException();
+    private int getIdx(int row, int col) {
+        if (row < 1 || col < 1 || row > n || col > n)
+            throw new IllegalArgumentException();
 
-		return n*(row-1) + col-1;
-	}
+        return n*(row-1) + col-1;
+    }
 
-	public    void open(int row, int col) {    // open site (row, col) if it is not open already
-		int idx = getIdx(row, col);
+    public    void open(int row, int col) {    // open site (row, col) if it is not open already
+        int idx = getIdx(row, col);
 
-		if (isOpen[idx])
-			return;
-		isOpen[idx] = true;
-		++nOpen;
+        if (isOpen[idx])
+            return;
+        isOpen[idx] = true;
+        ++nOpen;
 
-		if (idx >= n && isOpen[idx-n]) {
-			uf.union(idx, idx-n);
-		}
-		if (idx < isOpen.length-n && isOpen[idx+n]) {
-			uf.union(idx, idx+n);
-		}
-		if (idx > 0 && isOpen[idx-1]) {
-			uf.union(idx, idx-1);
-		}
-		if (idx < isOpen.length-1 && isOpen[idx+1]) {
-			uf.union(idx, idx+1);
-		}
-	}
+        if (idx >= n && isOpen[idx-n]) {
+            uf.union(idx, idx-n);
+        }
+        if (idx < isOpen.length-n && isOpen[idx+n]) {
+            uf.union(idx, idx+n);
+        }
+        if (idx > 0 && isOpen[idx-1]) {
+            uf.union(idx, idx-1);
+        }
+        if (idx < isOpen.length-1 && isOpen[idx+1]) {
+            uf.union(idx, idx+1);
+        }
+    }
 
-	public boolean isOpen(int row, int col) {  // is site (row, col) open?
-		return isOpen[getIdx(row, col)];
-	}
+    public boolean isOpen(int row, int col) {  // is site (row, col) open?
+        return isOpen[getIdx(row, col)];
+    }
 
-	public boolean isFull(int row, int col) {  // is site (row, col) full?
-		return uf.connected(getIdx(row, col), upper);
-	}
+    public boolean isFull(int row, int col) {  // is site (row, col) full?
+        return uf.connected(getIdx(row, col), upper);
+    }
 
-	public     int numberOfOpenSites() {      // number of open sites
-		return nOpen;
-	}
+    public     int numberOfOpenSites() {      // number of open sites
+        return nOpen;
+    }
 
-	public boolean percolates() {              // does the system percolate?
-		return uf.connected(upper, lower);
-	}
+    public boolean percolates() {              // does the system percolate?
+        return uf.connected(upper, lower);
+    }
 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Percolation p = new Percolation(Integer.parseInt(args[1]));
+        assert !p.percolates();
     }
     
 }
