d0c1731a022ed9dd18aa9e8943add8827aafa05a
Anatolii Sakhnik <sakhnik@gmail.com>
2017-07-13 22:52:35 +0300

    Refactor site

--- 1/Percolation/src/Percolation.java
+++ 1/Percolation/src/Percolation.java
@@ -16,10 +16,12 @@ public class Percolation {
 
     private final int n;
     private final int upper, lower;
-    private boolean[] isOpen;
+    private byte[] site;
+    private static final byte OPEN_FLAG = 1;
+    private static final byte HAS_TOP = 2;
+    private static final byte HAS_BOTTOM = 4;
     private int nOpen;
     private final WeightedQuickUnionUF uf;
-    private final WeightedQuickUnionUF uf2;
 
     public Percolation(int n) {               // create n-by-n grid, with all sites blocked
         if (n <= 0)
@@ -29,12 +31,9 @@ public class Percolation {
         lower = upper + 1;
         nOpen = 0;
 
-        isOpen = new boolean[n*n];
-        for (int i = 0; i < isOpen.length; ++i)
-            isOpen[i] = false;
+        site = new byte[n*n];
 
         uf = new WeightedQuickUnionUF(n*n + 2);
-        uf2 = new WeightedQuickUnionUF(n*n + 1);
     }
 
     private int getIdx(int row, int col) {
@@ -44,50 +43,53 @@ public class Percolation {
         return n*(row-1) + col-1;
     }
 
+    private boolean isOpen(int idx) {
+        return (site[idx] & OPEN_FLAG) != 0;
+    }
+
+    private void setOpen(int idx) {
+        site[idx] |= OPEN_FLAG;
+    }
+
     public    void open(int row, int col) {    // open site (row, col) if it is not open already
         int idx = getIdx(row, col);
 
-        if (isOpen[idx])
+        if (isOpen(idx))
             return;
-        isOpen[idx] = true;
+        setOpen(idx);
         ++nOpen;
 
         if (row > 1) {
-            if (isOpen[idx-n]) {
+            if (isOpen(idx-n)) {
                 uf.union(idx, idx-n);
-                uf2.union(idx, idx-n);
             }
         }
         else {
             uf.union(idx, upper);
-            uf2.union(idx, upper);
         }
 
         if (row < n) {
-            if (isOpen[idx+n]) {
+            if (isOpen(idx+n)) {
                 uf.union(idx, idx+n);
-                uf2.union(idx, idx+n);
             }
         } else {
             uf.union(idx, lower);
         }
 
-        if (col > 1 && isOpen[idx-1]) {
+        if (col > 1 && isOpen(idx-1)) {
             uf.union(idx, idx-1);
-            uf2.union(idx, idx-1);
         }
-        if (col < n && isOpen[idx+1]) {
+        if (col < n && isOpen(idx+1)) {
             uf.union(idx, idx+1);
-            uf2.union(idx, idx+1);
         }
     }
 
     public boolean isOpen(int row, int col) {  // is site (row, col) open?
-        return isOpen[getIdx(row, col)];
+        return isOpen(getIdx(row, col));
     }
 
     public boolean isFull(int row, int col) {  // is site (row, col) full?
-        return uf2.connected(getIdx(row, col), upper);
+        return uf.connected(getIdx(row, col), upper);
     }
 
     public     int numberOfOpenSites() {      // number of open sites
